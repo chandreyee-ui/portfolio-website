53412933723ec1c8312ef292a60fc439a1f1d827
chandreyee-ui <chandreyee.design@gmail.com>
2024-10-10 15:34:30 +0100

    2.3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert("Hello");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
